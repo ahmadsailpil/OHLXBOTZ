307c6a7bb0ccd04925f675c2e3d1957ecb6cbda7
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-21 06:28:07 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,21 +1,16 @@
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper' 
- import fetch from 'node-fetch' 
-  
- let handler = async (m, { conn, args, usedPrefix, command }) => { 
-     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243` 
-     const { author: { nickname }, video, description } = await tiktokdl(args[0]) 
-         .catch(async _ => await tiktokdlv2(args[0])) 
-         .catch(async _ => await tiktokdlv3(args[0])) 
-     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd 
-     if (!url) throw 'Can\'t download video!' 
-     conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(), 
-         url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [ 
-         ['ᴅᴏɴᴀꜱɪ', `.donasi`], 
-         ['ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`], 
-         [null, null]], m) 
- } 
- handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>') 
- handler.tags = ['downloader'] 
- handler.command = /^(tik(tok)?(tok)?(dl)?)$/i 
-  
- export default handler
+const { tiktokdl, tiktokdlv2, tiktokdlv3 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
+	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
+    tiktokdlv3(args[0]).then(r => {
+    let video = r.video.no_watermark
+    conn.sendFile(m.chat, video, '', `*${wm}*`, m)
+    })
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.limit = true
+handler.group = true
+
+handler.command = /^(tt|tiktok|tik)$/i
+
+module.exports = handler
